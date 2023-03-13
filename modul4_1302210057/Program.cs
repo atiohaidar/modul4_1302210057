4e405f727a10921b8d3d80a99a48fd87ebe9beb2
tio <atiohaidar@gmail.com>
2023-03-13 07:49:46 +0700

    menambahkan class PosisiKarakterGame

--- modul4_1302210057/Program.cs
+++ modul4_1302210057/Program.cs
@@ -1,13 +1,58 @@
-﻿public class KodeBuah {
-    public enum Buah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka };
-    public static String getKodeBuah(Buah buah)
+﻿public class Program
+{
+    enum State { Tengkurap, Jongkok, Berdiri, Terbang };
+    public static void Main()
     {
-        String[] kode_buah = { "A00", "B00", "C00", "D00", " E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
-        return kode_buah[(int)buah];
+        State state = State.Tengkurap;
+        //String[] output = { "Pintu terkunci", "Pintu tidak terkunci" };
+        while (true)
+        {
+            Console.WriteLine(state);
+            Console.Write("Enter Command : ");
 
+            string command = Console.ReadLine();
+            if (command == "TombolS")
+            {
+                Console.WriteLine("tombol arah bawah ditekan");
+            }else if (command== "TombolW") {
+                Console.WriteLine("tombol arah atas ditekan");
+
+            }
+            switch (state)
+            {
+                case State.Tengkurap:
+                    if (command == "TombolW")
+                        state = State.Jongkok;
+
+                    break;
+                case State.Jongkok:
+
+                    if (command == "TombolS")
+                        state = State.Tengkurap;
+                    else if (command == "TombolW")
+                    {
+                        state = State.Berdiri;
+                    }
+                    break;
+                case State.Berdiri:
+                    if (command == "TombolS")
+                        state = State.Jongkok;
+                    else if (command == "TombolW")
+                        state = State.Terbang;
+
+                    break;
+                case State.Terbang:
+                    if(command == "TombolX")
+                    {
+                        state = State.Jongkok;
+
+                    }else if (command == "TombolS")
+                    {
+                        state = State.Berdiri;
+                    }
+                    break;
+
+            }
+        }
     }
-    static void Main()
-    {
-        Buah buah = Buah.Semangka;
-        Console.WriteLine(getKodeBuah(buah));
-    } }
+}
